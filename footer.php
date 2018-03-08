8148e0a2e6d6b111c9d1214236c5c30461fc2e5a
Gaurav Panchal <gaurav.panchal@sooryen.com>
2018-03-08 15:55:46 +0530

    version fix

--- footer.php
+++ footer.php
@@ -23,7 +23,7 @@ $footer_type = get_option('footer_type') == '' ? 'navbar-inverse' : get_option('
 </div><!-- #page -->
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.1/jquery.min.js'></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js'></script>
-<script src='https://cdn.rawgit.com/grvpanchal/bootstrap-essentials/v0.4.0/dist/js/bootstrap-essentials.min.js'></script>
+<script src='https://cdn.rawgit.com/grvpanchal/bootstrap-essentials/v0.6.0/dist/js/bootstrap-essentials.min.js'></script>
 <?php wp_footer(); ?>
 
 </body>
